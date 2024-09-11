0ac63d6cdea2597a9c91f3152c49837b76ab19a9
João Rangel <jpar.dev00@gmail.com>
2024-09-11 01:01:05 -0300

    Implementando Código No Repositório Remoto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia("111", "n1", "11113222" , 8);
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Telegram");
+
+Console.WriteLine("___________________");
+Console.WriteLine();
+
+Iphone i9 = new Iphone("222", "i9", "333333333" , 16);
+i9.Ligar();
+i9.ReceberLigacao();
+i9.InstalarAplicativo("WhatsApp");
+
